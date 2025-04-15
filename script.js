3982c5eb539f5d99d6bf65e226dec16f2b540416
unknown <mparker0693@gmail.com>
2025-04-14 21:24:04 -0500

    'redesign'

---
+++ script.js
@@ -0,0 +1,17 @@
+function openTab(event, tabName) {
+    // Get all elements with class="tab-content" and hide them
+    var tabContents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].style.display = "none";
+    }
+
+    // Get all elements with class="tab-button" and remove the class "active"
+    var tabButtons = document.getElementsByClassName("tab-button");
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
